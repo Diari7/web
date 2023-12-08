e59e3908ed57971921d840717e3c696df0627f72
Diari7 <149503192+Diari7@users.noreply.github.com>
2023-12-08 01:10:52 +0100

    Login.html Ourteam.html
    Login dhe our team ndryshimet

---
+++ login.js
@@ -0,0 +1,16 @@
+function login{
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var errorMessage = document.getElementById("error-message");
+
+    if(username === "user" && password === "password"){
+
+        alert("Login successful!");
+
+    }else {
+
+        errorMessage.innerHTML = "Invalid Username or pssword. Please try again.";
+    }
+
+}
